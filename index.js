2e7d63d6c18e3b7ce19c10898cd158be465ed77d
chi_duong <anhchictw311@gmail.com>
2022-11-07 15:43:23 +0700

    fix

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express'
 const app = express()
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const port = 8080
-
-
+app.use(express.static('public'))
 // parse application/x-www-form-urlencoded
 // parse application/json
 
-app.get('/',(req,res)=>{
+app.get('/chan',(req,res)=>{
     res.send(`success`)
 })
 
